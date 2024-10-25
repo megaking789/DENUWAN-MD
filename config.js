1f118a8e7a1dc6a4ffa75a99ad4c49876045a10d
megaking789 <dilababah4@gmail.com>
2024-10-25 14:18:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "43N3WBRD#JbZIX2sLqZEeEuJKhMx5SMHUsdFtCEe00YSH9pXkYoo",
 MONGODB: process.env.MONGODB || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://unitedcamps.in/Images/file_4270.jpg",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
